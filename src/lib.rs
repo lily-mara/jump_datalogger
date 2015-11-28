a8acdeeb037037f458f81e24215aed0b2171c5b2
Nate Mara <natemara@gmail.com>
2015-11-28 19:39:45 +0100

    change dump function to use f32 instead of f64

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,19 @@ use std::mem;
 mod test;
 
 const BYTES_IN_I32: usize = 4;
+const BYTES_IN_F32: usize = 4;
 const BYTES_IN_F64: usize = 8;
 
-pub fn load_data_file(filename: &str) -> Result<Vec<f64>, io::Error> {
+pub fn load_data_file(filename: &str) -> Result<Vec<f32>, io::Error> {
     let mut f = try!(File::open(filename));
     let num_bytes = try!(f.metadata()).len();
-    let num_records = num_bytes / BYTES_IN_F64 as u64;
-    let mut buf = [0u8; BYTES_IN_F64];
+    let num_records = num_bytes / BYTES_IN_F32 as u64;
+    let mut buf = [0u8; BYTES_IN_F32];
     let mut out = Vec::new();
 
     for _ in 0..num_records {
         try!(f.read(&mut buf));
-        out.push(u8_to_f64(buf));
+        out.push(u8_to_f32(buf));
     }
 
     Ok(out)
@@ -33,6 +34,13 @@ fn u8_to_i32(input: [u8; BYTES_IN_I32]) -> i32 {
 
 /// Convert a slice of 8 u8 numbers to a rust f64 number.
 fn u8_to_f64(input: [u8; BYTES_IN_F64]) -> f64 {
+    unsafe {
+        mem::transmute(input)
+    }
+}
+
+/// Convert a slice of 4 u8 numbers to a rust f32 number.
+fn u8_to_f32(input: [u8; BYTES_IN_F32]) -> f32 {
     unsafe {
         mem::transmute(input)
     }
--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,10 @@
-use super::{ u8_to_i32, u8_to_f64 };
-use std::{ i32, f64 };
+use super::{ u8_to_i32, u8_to_f64, u8_to_f32 };
+use std::{ i32, f64, f32 };
 
 const MAX_I32: [u8; 4] = [0xFF, 0xFF, 0xFF, 0x7F];
 const MIN_I32: [u8; 4] = [0x00, 0x00, 0x00, 0x80];
+const MAX_F32: [u8; 4] = [0xFF, 0xFF, 0x7F, 0x7F];
+const MIN_F32: [u8; 4] = [0xFF, 0xFF, 0x7F, 0xFF];
 const MAX_F64: [u8; 8] = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xEF, 0x7F];
 const MIN_F64: [u8; 8] = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xEF, 0xFF];
 
@@ -49,4 +51,19 @@ fn test_f64_max() {
 #[test]
 fn test_f64_min() {
     assert_eq!(u8_to_f64(MIN_F64), f64::MIN);
+}
+
+#[test]
+fn test_pi_f32() {
+    assert_eq!(u8_to_f32([0xDA, 0x0F, 0x49, 0x40]), 3.1415926);
+}
+
+#[test]
+fn test_f32_max() {
+    assert_eq!(u8_to_f32(MAX_F32), f32::MAX);
+}
+
+#[test]
+fn test_f32_min() {
+    assert_eq!(u8_to_f32(MIN_F32), f32::MIN);
 }
